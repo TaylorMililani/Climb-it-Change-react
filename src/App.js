464eee4e0bb7aada17ce7f7ec8b01e972c493af5
Taylor Tompkins <taylormililani@gmail.com>
2021-02-07 19:19:28 -0800

    added router

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const BASE_URL = 'http://127.0.0.1:5000/'
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
